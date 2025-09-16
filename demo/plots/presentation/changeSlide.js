49b5e3a95e14f65159c22d07ab4f92626341e888
Facundo <facundomrb@gmail.com>
2025-09-16 13:42:16 -0300

    Slide de contacto

--- demo/plots/presentation/changeSlide.js
+++ demo/plots/presentation/changeSlide.js
@@ -4,7 +4,7 @@ document.addEventListener("keydown", function(event) {
     if (!match) return; // Si no coincide, no hace nada
 
     let currentPage = parseInt(match[1]);
-    totalPages = 18;
+    totalPages = 19;
 
     if (event.key === "ArrowRight" && currentPage < totalPages) {
       // Navega a la siguiente página
